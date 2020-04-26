7a8e365bd28e556e7c58850b4e463fc169e123fc
Zhenghao Jia <cherie-xia@hotmail.com>
2020-04-26 23:00:45 +0800

    small fix

--- 04-reduxsaga-localstorage-jsonserver/src/index.js
+++ 04-reduxsaga-localstorage-jsonserver/src/index.js
@@ -27,13 +27,27 @@ if (window.localStorage) {
   if (localStorage.getItem("lists") === null) {
     const array = [
       [
-        "更新一，首次登陆，尝试从浏览器的localStorage读取内容，如果浏览器不支持，则回到以前的模式",
+        "使用tip1，首次登陆，尝试从浏览器的localStorage读取内容，如果浏览器不支持，则每次记录的内容无法保存，下次登陆会重置",
         1,
       ],
-      ["更新二，加入了localStorage和JSON server切换按钮", 1],
-      ["更新三，如果使用了localStorage，则每次登陆（内容不丢失）", 1],
       [
-        "更新四，此类数据存在本地浏览器中，对个人安全，但即使同一台电脑，跨多个浏览器，存储也是不同的",
+        "使用tip2，如果浏览器有localStorage，则每次登陆，内容不丢失，支持localStorage的浏览器，请查询https://caniuse.com/#search=localStorage",
+        1,
+      ],
+      [
+        "使用tip3，左键某条代办记录，该记录的状态会在active和completed之间切换，右键，会删除",
+        1,
+      ],
+      [
+        "使用tip4，右键删除的记录会被放到右侧的回收区，点击垃圾回收图标，永久删除，点击cloud图标，则该记录会被还原且置顶",
+        1,
+      ],
+      [
+        "使用tip5，输入框除了可以add记录，也可以搜索某条记录，适用于代办过多的情况",
+        1,
+      ],
+      [
+        "使用tip6，所有数据存在本地浏览器中，对个人安全，但即使同一台电脑，跨多个浏览器，存储是不同的",
         1,
       ],
     ];
